a0f2322b1033ef208769487b5261e32fdfeefdd0
Davi Miranda <davimiranda210@gmail.com>
2024-09-12 08:56:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
-from matriz import preencher_matriz_com_input
+from matriz import Matriz
 from calculos import Calculos
 
 def main():
-    print("MATRIZ 1:---------------------")
-    matriz1 = preencher_matriz_com_input()
+    linhas = int(input("Digite o número de linhas: "))
+    colunas = int(input("Digite o número de colunas: "))
 
-    print("MATRIZ 2:---------------------")
-    matriz2 = preencher_matriz_com_input()
-    
+    # Criando duas instâncias de Matriz
+    matriz1 = Matriz(linhas, colunas)
+    matriz2 = Matriz(linhas, colunas)
+
+    # Preenchendo as matrizes com valores do usuário
+    print("Preencha a primeira matriz:")
+    matriz1.preencher_matriz_com_input()
+
+    print("Preencha a segunda matriz:")
+    matriz2.preencher_matriz_com_input()
+
+    # Instanciando a classe Calculos
     calc = Calculos(matriz1, matriz2)
+
+    # Exibindo a matriz
+    print("\nPrimeira Matriz preenchida:")
+    print(matriz1)
+    print("\nSegunda Matriz preenchida:")
+    print(matriz2)
+
+    acao = input("Digite a ação (+ para soma, * para multiplicação): ")
     
-    acao = input("Digite a ação: ")
-    
-    if (acao == '+'): #soma
+    if acao == '+':
         result = calc.soma()
+        print("Resultado da soma:")
         print(result)
-    elif (acao == 'D'): # Multiplicação
+    elif acao == '*':
         result = calc.multiplicacao()
+        print("Resultado da multiplicação:")
         print(result)
-        pass
-    elif (acao == '*'):
-        pass
-    
-    
-    
-main()
+    else:
+        print("Ação não reconhecida.")
+
+if __name__ == "__main__":
+    main()
