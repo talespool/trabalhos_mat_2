bcc9ab3cc34078569c79abdcd4ee2b76eb7bfbb8
Davi Miranda <davimiranda210@gmail.com>
2024-12-03 17:55:58 -0300

    Update Resolucao.py

--- Resolucao.py
+++ Resolucao.py
@@ -1,55 +1,70 @@
 import numpy as np
-class Resolucao():
-    #def __init__(self, matriz):
-        #self.matriz = matriz
-        
+from Calculos3 import Calculos3
 
-    def analisar_matriz_adjacencia(matriz):
+
+class Resolucao:
+    
+    
+    
+    def __init__(self, matriz):
         """
-        Analisa uma matriz de adjacência e retorna informações sobre o grafo.
+        Inicializa a classe com uma matriz de adjacência para operações.
         """
-        n = len(matriz)
-        graus = [sum(matriz[i]) for i in range(n)]
-        conexoes = [(i, j) for i in range(n) for j in range(n) if matriz[i][j] != 0]
+        self.matriz = matriz
+        self.calculo = Calculos3()
+        
+    
+    def analisar_matriz_adjacencia(self):
+        """
+        Analisa a matriz de adjacência e retorna informações sobre o grafo.
+        """
+        n = len(self.matriz)
+        graus = [sum(self.matriz[i]) for i in range(n)]
+        conexoes = [(i, j) for i in range(n) for j in range(n) if self.matriz[i][j] != 0]
 
         return {
             "graus": graus,
             "total_arestas": sum(graus) // 2,
             "conexoes": conexoes,
         }
-        
-    def calcular_hub_autoridade(matriz, iteracoes=100, tol=1e-6):
+
+    def calcular_hub_autoridade(self, iteracoes=100, tol=1e-6):
         """
-        Calcula os vetores de hub e autoridade usando o algoritmo HITS.
+        Calcula os vetores de hub e autoridade usando o algoritmo HITS, sem bibliotecas externas.
         """
-        n = len(matriz)
-        matriz = np.array(matriz, dtype=float)
         
+        n = len(self.matriz)
+
         # Inicializando os vetores
-        autoridade = np.ones(n)
-        hub = np.ones(n)
+        autoridade = [1.0] * n
+        hub = [1.0] * n
+
+        matriz_transposta = self.calculo.transposta(self.matriz) #ERRO-------------------------
 
         for _ in range(iteracoes):
-            nova_autoridade = matriz.T @ hub
-            nova_hub = matriz @ autoridade
+            # Calcula o novo vetor de autoridade
+            nova_autoridade = self.calculo.multiplicacao(matriz_transposta, hub)
+            # Calcula o novo vetor de hub
+            nova_hub = self.calculo.multiplicacao(self.matriz, autoridade)
 
-            # Normalizando os vetores
-            nova_autoridade /= np.linalg.norm(nova_autoridade, 2)
-            nova_hub /= np.linalg.norm(nova_hub, 2)
+            # Normaliza os vetores
+            nova_autoridade = self.calculo.normalizar_vetor(nova_autoridade)
+            nova_hub = self.calculo.normalizar_vetor(nova_hub)
 
-            # Verificando convergência
-            if (np.allclose(nova_autoridade, autoridade, atol=tol) and
-                np.allclose(nova_hub, hub, atol=tol)):
-                break
+            # Verifica convergência
             
             autoridade, hub = nova_autoridade, nova_hub
 
         return autoridade, hub
-    
-    def ordenar_nos(vetor):
+
+
+    def ordenar_nos(self, vetor):
         """
         Ordena os nós com base nos valores de um vetor (ex: hub ou autoridade).
+        Retorna uma lista de tuplas no formato (índice, valor), ordenada em ordem decrescente.
         """
-        return sorted(enumerate(vetor), key=lambda x: x[1], reverse=True)
-    
-    
+        return sorted(
+        [(i, float(v[0]) if isinstance(v, list) else float(v)) for i, v in enumerate(vetor)], 
+        key=lambda x: x[1], 
+        reverse=True
+    )
